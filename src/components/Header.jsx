23e8a8fbaac47add5399d156acd6560080cdac39
KuntalRathod <nature.kr70@gmail.com>
2024-05-14 03:30:38 +0530

    feat: Add lazy loading for images in Gif component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [categories, setCategories] = useState([])
   const [showCategories, setShowCategories] = useState(false)
 
-  console.log(categories,setCategories);
+  console.log(categories, setCategories)
 
   const { giphy, filter, setFilter, favourites } = GifState()
 
@@ -30,15 +30,14 @@ const Header = () => {
           </h1>
         </Link>
 
-        <div className="font-bold text-md flex gap-1 items-center">
-          {/* Render Categories */}
-
+        {/* Render Categories */}
+        <div className="font-bold text-md flex gap-2 items-center">
           {categories?.slice(0, 5)?.map((category) => {
             return (
               <Link
                 key={category.name}
                 to={`/${category.name_encoded}`}
-                className="px-1 py-1 hover:gradient border-b-4 hidden lg:block"
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
               >
                 {category.name}
               </Link>
@@ -48,13 +47,14 @@ const Header = () => {
           <button onClick={() => setShowCategories(!showCategories)}>
             <HiEllipsisVertical
               size={35}
-              className={`py-0.5 hover:gradient ${showCategories ? "gradient" : ""} border-b-4 hidden lg:block`}
+              className={`py-0.5 hover:gradient ${showCategories ? "gradient" : ""} border-b-4 cursor-pointer hidden lg:block`}
             />
           </button>
-          <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
-            <Link to="/favourites"> Favourite GIFs</Link>
-          </div>
-
+          {favourites.length > 0 && (
+            <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+              <Link to="/favourites"> Favourite GIFs</Link>
+            </div>
+          )}
           <button>
             <HiMiniBars3BottomRight
               size={35}
@@ -65,10 +65,20 @@ const Header = () => {
 
         {showCategories && (
           <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient z-20">
-            <span>Categories</span>
-            <hr />
-            <div>
-              <Link className="font-bold">Reactions</Link>
+            <span className="text-4xl font-extrabold">Categories</span>
+            <hr className="bg-gray-100 opacity-50 my-5" />
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {categories?.map((category) => {
+                return (
+                  <Link
+                    key={category.name}
+                    to={`/${category.name_encoded}`}
+                    className="font-bold"
+                  >
+                    {category.name}
+                  </Link>
+                )
+              })}
             </div>
           </div>
         )}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { GifState } from "../context/GifContext"
+import Gif from "../components/Gif"
 
 const Home = () => {
-  return <div>Home</div>
+  const { giphy, gifs, filter, setGifs, favourites } = GifState()
+
+  const fetchTrendingGIFs = async () => {
+    const { data } = await giphy.trending({
+      limit: 20,
+      type: filter,
+      rating: "g",
+    })
+
+    setGifs(data)
+  }
+
+  useEffect(() => {
+    fetchTrendingGIFs()
+  }, [filter])
+
+  return (
+    <div>
+      <img
+        src="/banner.gif"
+        alt="earth banner"
+        className="mt-2 rounded w-full"
+      />
+      {/* <FilterGif /> */}
+
+      <div>
+        {gifs.map((gif) => {
+          return <Gif gif={gif} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
