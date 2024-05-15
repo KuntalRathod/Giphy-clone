a8304409ef40507efc34046cf0215eeea18c5fb3
KuntalRathod <nature.kr70@gmail.com>
2024-05-15 15:17:21 +0530

    feat: Update GifSearch component to include search on Enter key press

--- src/components/GifSearch.jsx
+++ src/components/GifSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { HiOutlineMagnifyingGlass } from "react-icons/hi2"
+import { HiMiniXMark, HiOutlineMagnifyingGlass } from "react-icons/hi2"
 import { useNavigate } from "react-router-dom"
 
 const GifSearch = () => {
@@ -14,15 +14,30 @@ const GifSearch = () => {
     navigate(`/search/${query}`)
   }
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchGIFs()
+    }
+  }
+
   return (
     <div className="flex relative ">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="Search all the GIFs and Stickers"
         className="w-full pl-4 pr-14 py-5 text-xl text-black rounded-tl rounded-bl border border-gray-300 outline-none"
       />
+      {query && (
+        <button
+          onClick={() => setQuery("")}
+          className="absolute bg-gray-300 opacity-90 rounded-full right-20 mr-2 top-6"
+        >
+          <HiMiniXMark size={22} />
+        </button>
+      )}
       <button
         onClick={searchGIFs}
         className="bg-gradient-to-tr from-pink-600 to-pink-400 text-white px-4 py-2 rounded-tr rounded-br"
